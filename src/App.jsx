89cc790e39beeaaf15f84c457ba99ab71315119f
Kyrylko <kkuzmenko1992@gmail.com>
2024-07-27 12:08:03 -0230

    added basic markup and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Description from './components/Description/Description'
+import Options from './components/Options/Options'
+import Feedback from './components/Feedback/Feedback'
 
 function App() {
 
   return (
     <>
       <div>
-
+        <Description />
+      </div>
+      <div>
+        <Options />
+      </div>
+      <div>
+        <Feedback />
       </div>
     </>
   )
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,1 +1,17 @@
 import styles from "./Feedback.module.css"
+
+const Feedback = () => {
+    return (
+        <div>
+            <ul>
+                <li>Good: </li>
+                <li>Neutral: </li>
+                <li>Bad: </li>
+                <li>Total: </li>
+                <li>Pozitive: </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Feedback;
